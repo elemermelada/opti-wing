cda0ab8913984e7730a56054e8b1f8081690ddbf
elemermelada <iiasanmi9@gmail.com>
2022-10-13 20:09:40 +0200

    INIT

---
+++ Vfuel.m
@@ -0,0 +1,8 @@
+%% FUNCTION TO OBTAIN THE EXCESS FUEL VOLUME TO COMPLETE THE MISSION
+%TODO - make sure that necessary fuel is only evaluated ONCE per iteration...
+function [vol] = Vfuel(x)
+%get necessary fuel
+%get available fuel vol
+vol = 1;
+end
+
---
+++ Wto.m
@@ -0,0 +1,9 @@
+%% FUNCTION TO OBTAIN Wto FROM THE DESIGN VECTOR
+function [W] = Wto(x)
+%get aero eficiency
+%get breguet
+%get aero loads
+%get structural weight
+W = x(1);
+end
+
