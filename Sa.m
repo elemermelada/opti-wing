da7f988b6427ffe0282a6a90747b72ef86e58217
Elemer San Miguel <iiasanmi9@gmail.com>
2022-12-14 14:03:29 +0100

    cleaning

--- Sa.m
+++ Sa.m
@@ -3,7 +3,7 @@ function [S] = Sa(a)
 n = size(a,2);
 S = @(x) 0;
 for i=1:n
-    S = @(x) S(x) + nchoosek(n-1,i-1).*a(i)*x.^(i-1)*(1-x)^(n-i);
+    S = @(x) S(x) + nchoosek(n-1,i-1).*a(i)*x.^(i-1).*(1-x).^(n-i);
 end
 end
 
