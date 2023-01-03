f2b1c8e349f75361198853f2541708f46024f0a7
Elemer San Miguel <iiasanmi9@gmail.com>
2023-01-03 09:48:42 +0100

    Update outputFcn.m

--- outputFcn.m
+++ outputFcn.m
@@ -3,4 +3,5 @@ function stop = outputFcn(x,optimValues,state,fileID)
     fprintf(fid, "%s\n", "probando");
     fclose(fid);
     fprintf(fileID,'%u,%u,%4.2f \r\n',optimValues.iteration,optimValues.funccount, optimValues.fval)
+    stop = false;
 end
